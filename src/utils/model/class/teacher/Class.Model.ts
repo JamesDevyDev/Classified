61bb014d70e78445d35947c5f102d7147f1a55e8
James Talamo <jamestalamo4@gmail.com>
2025-10-28 21:05:57 +0800

    added class model

---
+++ src/utils/model/class/teacher/Class.Model.ts
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+import "../../users/teacher/Teacher.model"
+import "../../users/student/Student.model";
+
+const classSchema = new mongoose.Schema({
+    teacherId: {
+        ref: "Teacher",
+        type: mongoose.Types.ObjectId
+    },
+    course: {
+        type: String,
+        required: true
+    },
+    dayOfWeek: {
+        type: String,
+        default: "",
+        enum: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+        required: true
+    },
+    startTime: {
+        type: String,
+        required: true
+    },
+    endTime: {
+        type: String,
+        required: true
+    },
+    students: {
+        type: Number,
+    },
+    color: {
+        type: String,
+        enum: ["cyan", "purple", "emerald", "amber", "rose", "blue"],
+        default: "cyan"
+    },
+    studentList: {
+        type: [mongoose.Schema.Types.ObjectId],
+        ref: "Student", 
+        default: [] 
+    }
+})
+
+const Class = mongoose.models.Class || mongoose.model("Class", classSchema)
+
+export default Class
